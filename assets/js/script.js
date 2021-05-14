3b2c92cd446d7565c2348aeac95d22dc6177a09f
JoseGallardoCasanova <jose.d.gallardo.c@gmail.com>
2021-05-14 15:22:54 -0400

    se agrega modal y se termina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
 
     $("a").click(function (event) {
-        if (this.hash !== "") {  
-            event.preventDefault();          
-            var gato = this.hash;            
+        if (this.hash !== "") {
+            event.preventDefault();
+            var gato = this.hash;
             $('html, body').animate({
                 scrollTop: $(gato).offset().top
-            }, 800, function () {                
+            }, 800, function () {
                 window.location.hash = gato;
             })
-        }      
+        }
     })
 
-    $(window).scroll(function(){
-        if($("#menu").offset().top > 300){
+    $(window).scroll(function () {
+        if ($("#menu").offset().top > 300) {
             $("#menu").addClass("bg-dark");
         } else {
             $("#menu").removeClass("bg-dark");
         }
     });
+
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myModal').trigger('focus')
+    })
+
 
 })
 
